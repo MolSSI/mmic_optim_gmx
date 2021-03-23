85db7198b017a49b416bbdcdeeabb9a0167b81c3
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-23 14:45:51 -0400

    import components

---
+++ mmic_optim_gmx/components/__init__.py
@@ -0,0 +1,7 @@
+from . import gmx_preprocess_component
+from . import gmx_compute_component
+from . import gmx_post_component
+
+from .gmx_compute_component import *
+from .gmx_compute_component import *
+from .gmx_post_component import *
