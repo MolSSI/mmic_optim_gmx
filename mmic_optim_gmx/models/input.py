236b4804ac85bc39705470c50cdbab92a3f7e725
Xu Guo <32465720+RlyehAD@users.noreply.github.com>
2021-03-24 16:43:54 -0400

    Update input.py
    Add description for mdp_file and struct_file

--- mmic_optim_gmx/models/input.py
+++ mmic_optim_gmx/models/input.py
@@ -1,7 +1,8 @@
 from mmelemental.models.base import ProtoModel
+from pydantic import Field
 
 
 class GmxComputeInput(ProtoModel):
     proc_input: "ProcInput"
-    mdp_file: str
-    struct_file: str
+    mdp_file: str = Field(..., description="The file used for specifying the parameters. Should be a .mdp file")
+    struct_file: str = Field(..., description="The file of the system structure. Should be a .top file")
