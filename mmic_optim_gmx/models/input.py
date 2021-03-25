2cc59e1fe3f3d571c590cdcb965688b76fbdce6e
Xu Guo <32465720+RlyehAD@users.noreply.github.com>
2021-03-25 01:48:50 -0400

    Update input.py
    import ProcInput

--- mmic_optim_gmx/models/input.py
+++ mmic_optim_gmx/models/input.py
@@ -1,8 +1,9 @@
 from mmelemental.models.base import ProtoModel
+from mmelemental.models.proc.base import ProcInput
 from pydantic import Field
 
 
 class GmxComputeInput(ProtoModel):
-    proc_input: "ProcInput"
+    proc_input: ProcInput
     mdp_file: str = Field(..., description="The file used for specifying the parameters. Should be a .mdp file")
     struct_file: str = Field(..., description="The file of the system structure. Should be a .top file")
