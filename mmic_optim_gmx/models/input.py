7b164dea510adb82a8766c313db0017f2d9d769b
Xu Guo <32465720+RlyehAD@users.noreply.github.com>
2021-03-25 22:56:02 -0400

    Update input.py
    ProtoModel is changed to ProcInput; Corrected the input type to OptimInput

--- mmic_optim_gmx/models/input.py
+++ mmic_optim_gmx/models/input.py
@@ -1,9 +1,10 @@
 from mmelemental.models.base import ProtoModel
 from mmelemental.models.proc.base import ProcInput
+from mmic_optim.models import OptimInput
 from pydantic import Field
 
 
-class GmxComputeInput(ProtoModel):
-    proc_input: ProcInput = Field(..., description="Procedure input schema.")
+class GmxComputeInput(ProcInput):
+    proc_input: OptimInput = Field(..., description="Procedure input schema.")
     mdp_file: str = Field(..., description="The file used for specifying the parameters. Should be a .mdp file.")
     struct_file: str = Field(..., description="The file of the system structure. Should be a .top file.")
