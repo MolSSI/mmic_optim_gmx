069fdc7a4d6f703bf7318bf8fd696b09ea02d630
Xu Guo <32465720+RlyehAD@users.noreply.github.com>
2021-03-26 07:09:44 -0400

    Update gmx_preprocess_component.py
    Added the function to transfer Molecule to a pdb file

--- mmic_optim_gmx/components/gmx_preprocess_component.py
+++ mmic_optim_gmx/components/gmx_preprocess_component.py
@@ -1,12 +1,14 @@
 # Import models
+from mmelemental.models.util.output import FileOutput
 from mmic_optim.models.input import OptimInput
 from ..models import GmxComputeInput
 
 # Import components
+from mmic_util.components import CmdComponent
 from mmic.components.blueprints import SpecificComponent
 
 from typing import List, Tuple, Optional, Set
-
+import os
 
 __all__ = ["GmxPreProcessComponent"]
 
@@ -14,6 +16,8 @@ __all__ = ["GmxPreProcessComponent"]
 class GmxPreProcessComponent(SpecificComponent):
     """
     Prepares input for running gmx energy minimization.
+    The Molecule object will be converted to a .pdb file here.
+    .mdp and .top files will be constructed.
     """
 
     @classmethod
@@ -32,7 +36,15 @@ class GmxPreProcessComponent(SpecificComponent):
         scratch_name: Optional[str] = None,
         timeout: Optional[int] = None,
     ) -> Tuple[bool, GmxComputeInput]:
-
+        
+        if isinstance(inputs, dict):
+            inputs = self.input()(**inputs)
+        
+        mols = inputs.molecule #Dict[str, Molecule]
+        
+        fname = "GMX_pre.pdb"
+        mols.to_file(fname)
+        
         # Parse GMX input params from inputs and create GmxComputeInput object
         gmx_compute = GmxComputeInput(inputs=..., ...)
         return True, GmxComputeInput(**gmx_compute)
