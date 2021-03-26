7e892f12db5a85af41f2f31b71b6913cbaccee61
Xu Guo <32465720+RlyehAD@users.noreply.github.com>
2021-03-26 07:38:28 -0400

    Update input.py
    Added a field of integrator

--- mmic_optim_gmx/models/input.py
+++ mmic_optim_gmx/models/input.py
@@ -2,10 +2,11 @@ from mmelemental.models.base import ProtoModel #Is this line necessary?
 from mmelemental.models.proc.base import ProcInput
 from mmic_optim.models import OptimInput
 from pydantic import Field
+from typing import Optional
 
 
 class GmxComputeInput(ProcInput):
     proc_input: OptimInput = Field(..., description="Procedure input schema.")
     mdp_file: str = Field(..., description="The file used for specifying the parameters. Should be a .mdp file.")
     struct_file: str = Field(..., description="The file of the system structure. Should be a .top file.")
-    integrator: str = Field(..., description="The algorithm used to implement energy minimization.")
+    integrator: Optional[str] = Field(None, description="The algorithm used to implement energy minimization.")
