d7261e0fdfa2a6c0060992a98afdfcbae951283e
Xu Guo <32465720+RlyehAD@users.noreply.github.com>
2021-03-26 07:42:29 -0400

    Update input.py
    delete integrator since OptimInput has it.

--- mmic_optim_gmx/models/input.py
+++ mmic_optim_gmx/models/input.py
@@ -9,4 +9,4 @@ class GmxComputeInput(ProcInput):
     proc_input: OptimInput = Field(..., description="Procedure input schema.")
     mdp_file: str = Field(..., description="The file used for specifying the parameters. Should be a .mdp file.")
     struct_file: str = Field(..., description="The file of the system structure. Should be a .top file.")
-    integrator: Optional[str] = Field(None, description="The algorithm used to implement energy minimization.")
+   
