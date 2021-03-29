1d9814dfee90f67efc86c1768a3a5ac589fc47e4
Glacier <guo12@iu.edu>
2021-03-28 21:50:38 -0400

    Edited the descriptions for molecule and trajectory to be consistenti with mmic_ffpa_gmx/mmic_ffpa_gmx/models/output.py

--- mmic_optim_gmx/models/output.py
+++ mmic_optim_gmx/models/output.py
@@ -3,5 +3,5 @@ from mmelemental.models.base import ProtoModel
 
 class GmxComputeOutput(ProtoModel):
     proc_input: OptimInput = Field(..., description="Procedure input schema.")
-    molecule: str = Field(..., description="File name of the molecule file")
-    trajectory: str = Field(..., description="File name of the trajectory file")
+    molecule: str = Field(..., description="Trajectory file string object")
+    trajectory: str = Field(..., description="Molecule file string object.")
