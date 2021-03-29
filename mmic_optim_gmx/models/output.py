90a333683f019e08e61bcfd5b2741d1dde321ab0
Glacier <guo12@iu.edu>
2021-03-28 21:44:44 -0400

    Updated output.py in models. Added the type for proc_input and wrote the decription for molecule and trajectory

--- mmic_optim_gmx/models/output.py
+++ mmic_optim_gmx/models/output.py
@@ -2,6 +2,6 @@ from mmelemental.models.base import ProtoModel
 
 
 class GmxComputeOutput(ProtoModel):
-    proc_input: "ProcInput"
-    molecule: str
-    trajectory: str
+    proc_input: OptimInput = Field(..., description="Procedure input schema.")
+    molecule: str = Field(..., description="File name of the molecule file")
+    trajectory: str = Field(..., description="File name of the trajectory file")
