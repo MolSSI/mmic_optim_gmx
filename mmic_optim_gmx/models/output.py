003ea2a3384aef0a2f10869cd02bfdeec454e630
Glacier <guo12@iu.edu>
2021-03-31 11:03:25 -0400

    fixed typo

--- mmic_optim_gmx/models/output.py
+++ mmic_optim_gmx/models/output.py
@@ -3,5 +3,5 @@ from mmelemental.models.base import ProtoModel
 
 class GmxComputeOutput(ProtoModel):
     proc_input: OptimInput = Field(..., description="Procedure input schema.")
-    molecule: str = Field(..., description="Trajectory file string object")
-    trajectory: str = Field(..., description="Molecule file string object.")
+    molecule: str = Field(..., description="Molecule file string object")
+    trajectory: str = Field(..., description="Trajectory file string object.")
