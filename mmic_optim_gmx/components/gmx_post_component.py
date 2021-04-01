280e6b8b81c14a0a373d108cf2f26d38f175e26b
Glacier <guo12@iu.edu>
2021-04-01 17:48:17 -0400

    fixed typos; added mol parsing method

--- mmic_optim_gmx/components/gmx_post_component.py
+++ mmic_optim_gmx/components/gmx_post_component.py
@@ -4,9 +4,11 @@ from mmelemental.models import Molecule, Trajectory
 from ..models import GmxComputeOutput
 
 # Import components
+from mmic_util.components import CmdComponent
 from mmic.components.blueprints import SpecificComponent
 
 from typing import Dict, Any, List, Tuple, Optional
+import os
 
 
 class PostComponent(SpecificComponent):
@@ -27,20 +29,69 @@ class PostComponent(SpecificComponent):
         timeout: Optional[int] = None,
     ) -> Tuple[bool, OptimOutput]:
 
+        clean_files = []
         # get trajs from traj output file
         traj_file = inputs.trajectory  # split trajs into multiple files
-        trajs_files = ...
+        trajs_files = {"em_traj":traj_file}
         traj = {
             key: Trajectory.from_file(trajs_files[key])
             for key in inputs.proc_input.trajectory
         }
 
         # get mols from parsing struct output file
-        mol_file = inputs.molecule  # split mols into multiple files
-        mols_files = ...
+        mol_file = inputs.molecule  # split mols into multiple files. This line might be deleted later
+        mol_files = {}#abspath for split molecules
+
+        #Start to split the gro file by molecules
+        for key in inputs.proc_input.molecule:
+            input_model = {
+                "molname": key,
+                "gro_fname": mol_file#May be replaced by inpuuts.molecule
+            }
+            CmdComponent.compute(input_model)
+            mol_files[key] = os.path.abspath(key+".gro")
+
         mol = {
-            key: Molecule.from_file(mols_files[key]) 
-            for key in inputs.proc_input.mol
+            key: Molecule.from_file(mols_files[key])
+            for key in inputs.proc_input.molecule
         }
 
         return True, OptimOutput(proc_input=inputs.proc_input, molecule=mol, trajectory=traj)
+
+        def build_input(
+            self,
+            inputs: Dict[str, Any],
+            config: Optional["TaskConfig"] = None,
+            template: Optional[str] = None,
+            ) -> Dict[str, Any]:
+            
+            gro_fname = inpits["gro_fname"]#The gro file containing all molecules 
+            mol_fname = inouts["molname"] + ".gro"
+
+            env = os.environ.copy()
+
+            if config:
+            env["MKL_NUM_THREADS"] = str(config.ncores)
+            env["OMP_NUM_THREADS"] = str(config.ncores)
+
+            scratch_directory = config.scratch_directory if config else None
+
+            return {
+                "command": [
+                    "grep",
+                    "-c",
+                    inputs["molname"],
+                    gro_fname,
+                    ">>",
+                    mol_fname,
+                    "|",
+                    "grep",
+                    inputs["molname"],
+                    gro_fname,
+                    ">>",
+                    mol_fname
+                    ],
+                "outfiles": [mol_fname],
+                "scratch_directory": scratch_directory,
+                "environment": env,
+            }
