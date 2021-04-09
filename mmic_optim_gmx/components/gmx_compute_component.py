95037a0fce2d2dcf3e637355047cc915326efefe
XU GUO <guo12@iu.edu>
2021-04-09 01:35:27 -0400

    fixed the names of the classes

--- mmic_optim_gmx/components/gmx_compute_component.py
+++ mmic_optim_gmx/components/gmx_compute_component.py
@@ -11,7 +11,7 @@ from typing import Dict, Any, List, Tuple, Optional
 import os
 
 
-class PostComponent(SpecificComponent):
+class GmxComputeComponent(SpecificComponent):
     @classmethod
     def input(cls):
         return GmxComputeInput
