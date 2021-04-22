6b91b84364b95c8283b778f9b533b58f97a18ec6
Xu_Guo <guo12@iu.edu>
2021-04-22 19:33:19 -0400

    Match the single molecule input

--- mmic_optim_gmx/components/gmx_post_component.py
+++ mmic_optim_gmx/components/gmx_post_component.py
@@ -5,6 +5,7 @@ from ..models import GmxComputeOutput
 
 # Import components
 from mmic_util.components import CmdComponent
+from mmelemental.util.files import random_file
 from mmic.components.blueprints import SpecificComponent
 
 from typing import Dict, Any, List, Tuple, Optional
@@ -29,38 +30,35 @@ class GmxPostComponent(SpecificComponent):
         timeout: Optional[int] = None,
     ) -> Tuple[bool, OptimOutput]:
 
+        """
+        This method translate the output of em
+        to mmic schema. But right now it can only
+        be applied to single molecule conditions
+        """
+
         clean_files = []
-        # get trajs from traj output file
-        traj_file = inputs.trajectory  # split trajs into multiple files
-        trajs_files = {"em_traj": traj_file}
+
+        traj_file = inputs.trajectory  
+        traj_name = list(inputs.proc_input.trajectory)[0]
+        trajs_files = {traj_name: traj_file}
         traj = {
             key: Trajectory.from_file(trajs_files[key])
             for key in inputs.proc_input.trajectory
         }
         clean_files.append(traj_file)
 
-        # get mols from parsing struct output file
-        mol_file = (
-            inputs.molecule
-        )  # split mols into multiple files. This line might be deleted later
-        mol_files = {}  # abspath for split molecules
 
-        # Start to split the gro file by molecules
-        for key in inputs.proc_input.molecule:
-            input_model = {
-                "molname": key,
-                "gro_fname": mol_file,  # May be replaced by inputs.molecule
-            }
-            cmd_input = self.build_input(input_model)
-            CmdComponent.compute(cmd_input)
-            #CmdComponent.compute(self.build_input(input_model))
-            mol_files[key] = os.path.abspath(key + ".gro")
-            clean_files.append(mol_files[key])
-
+        mol_file = inputs.molecule
+        mol_name = list(inputs.proc_input.molecule)[0]  
+        mol_files = {mol_name: mol_file} 
         mol = {
             key: Molecule.from_file(mols_files[key])
             for key in inputs.proc_input.molecule
-        }
+        }        
+        clean_files.append(mol_file)
+
+        self.cleanup(clean_files)
+
 
         return True, OptimOutput(
             proc_input=inputs.proc_input, molecule=mol, trajectory=traj
@@ -74,40 +72,4 @@ class GmxPostComponent(SpecificComponent):
             elif os.path.isfile(item):
                 os.remove(item)
 
-        def build_input(
-            self,
-            inputs: Dict[str, Any],
-            config: Optional["TaskConfig"] = None,
-            template: Optional[str] = None,
-        ) -> Dict[str, Any]:
 
-            gro_fname = inputs["gro_fname"]  # The gro file containing all molecules
-            mol_fname = inputs["molname"] + ".gro"
-
-            env = os.environ.copy()
-
-            if config:
-                env["MKL_NUM_THREADS"] = str(config.ncores)
-                env["OMP_NUM_THREADS"] = str(config.ncores)
-
-            scratch_directory = config.scratch_directory if config else None
-
-            return {
-                "command": [
-                    "grep",
-                    "-c",
-                    inputs["molname"],
-                    gro_fname,
-                    ">>",
-                    mol_fname,
-                    "|",
-                    "grep",
-                    inputs["molname"],
-                    gro_fname,
-                    ">>",
-                    mol_fname,
-                ],
-                "outfiles": [mol_fname],
-                "scratch_directory": scratch_directory,
-                "environment": env,
-            }
