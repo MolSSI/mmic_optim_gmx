2c42910b3a7239ddae7bea541a02aa3796419dc7
Xu_Guo <guo12@iu.edu>
2021-04-22 19:32:19 -0400

    Fix typo

--- mmic_optim_gmx/components/__init__.py
+++ mmic_optim_gmx/components/__init__.py
@@ -2,6 +2,6 @@ from . import gmx_preprocess_component
 from . import gmx_compute_component
 from . import gmx_post_component
 
-from .gmx_compute_component import *
+from .gmx_preprocess_component import *
 from .gmx_compute_component import *
 from .gmx_post_component import *
