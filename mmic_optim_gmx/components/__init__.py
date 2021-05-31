a6a61bdbcce223f6752bdc096e572de31e67868d
Xu_Guo <guo12@iu.edu>
2021-05-31 18:08:19 -0400

    format with black

--- mmic_optim_gmx/components/__init__.py
+++ mmic_optim_gmx/components/__init__.py
@@ -6,4 +6,8 @@ from .gmx_preprocess_component import *
 from .gmx_compute_component import *
 from .gmx_post_component import *
 
-__all__ = gmx_post_component.__all__ + gmx_compute_component.__all__ + gmx_preprocess_component.__all__
+__all__ = (
+    gmx_post_component.__all__
+    + gmx_compute_component.__all__
+    + gmx_preprocess_component.__all__
+)
