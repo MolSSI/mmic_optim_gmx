796f17d786f70e34f9aed319a4f7c13fa356261d
Xu_Guo <guo12@iu.edu>
2021-05-31 18:07:07 -0400

    Define __all__

--- mmic_optim_gmx/components/__init__.py
+++ mmic_optim_gmx/components/__init__.py
@@ -5,3 +5,5 @@ from . import gmx_post_component
 from .gmx_preprocess_component import *
 from .gmx_compute_component import *
 from .gmx_post_component import *
+
+__all__ = gmx_post_component.__all__ + gmx_compute_component.__all__ + gmx_preprocess_component.__all__
