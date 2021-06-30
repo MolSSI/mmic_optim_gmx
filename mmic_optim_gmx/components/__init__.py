bd647e0f979cf2eda0e5fbcfc77c80dbcf6ee8c1
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-06-30 12:44:29 -0400

    expose only OptimGmxComponent

--- mmic_optim_gmx/components/__init__.py
+++ mmic_optim_gmx/components/__init__.py
@@ -1,13 +1,3 @@
-from . import gmx_preprocess_component
-from . import gmx_compute_component
-from . import gmx_post_component
+from .gmx_optim_component import OptimGmxComponent
 
-from .gmx_preprocess_component import *
-from .gmx_compute_component import *
-from .gmx_post_component import *
-
-__all__ = (
-    gmx_post_component.__all__
-    + gmx_compute_component.__all__
-    + gmx_preprocess_component.__all__
-)
+__all__ = ["OptimGmxComponent"]
